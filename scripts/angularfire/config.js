ff3359a5419fc9e307883340f712089f49705ad9
TheodoreNguyen <theodore.h.nguyen@outlook.com>
2015-04-05 08:16:40 -0700

    uhm yeah?

--- scripts/angularfire/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://glowing-torch-4513.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password','anonymous','facebook'])
-
-  .constant('loginRedirectPath', '/login');
